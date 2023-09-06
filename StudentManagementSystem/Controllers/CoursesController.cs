656640feba24d9dc3b27ee5befd70de1bfae1c17
hoorasadat <mirsaanei@gmail.com>
2023-09-06 17:49:07 +0000

    add courses index

---
+++ StudentManagementSystem/Controllers/CoursesController.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StudentManagementSystem.BLL.Interfaces;
+
+namespace StudentManagementSystem.WEB.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CoursesController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        // GET: CoursesController
+        public async Task<ActionResult> Index()
+        {
+            IList<Course> courses = await _courseRepository.GetAllCourses();
+            return View(courses);
+        }
+
+        // GET: CoursesController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CoursesController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CoursesController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CoursesController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CoursesController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CoursesController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CoursesController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
